4d6c8874d619674cc52fbf28e4a83ceea91c5949
jellanb <jellan_15@hotmail.com>
2023-12-08 18:51:52 -0300

    add gelocalization to contact us method

--- js/custom.js
+++ js/custom.js
@@ -8,11 +8,46 @@ function getYear() {
 getYear();
 
 /** google_map js **/
-
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+function obtenerUbicacion() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(mostrarMapa, errorEnGeolocalizacion);
+    } else {
+        alert('La geolocalización no es soportada por este navegador.');
+    }
 }
+
+function mostrarMapa(posicion) {
+    const latitud = posicion.coords.latitude;
+    const longitud = posicion.coords.longitude;
+
+    const ubicacion = { lat: latitud, lng: longitud };
+
+    const mapa = new google.maps.Map(document.getElementById('googleMap'), {
+        center: ubicacion,
+        zoom: 15 // Puedes ajustar el nivel de zoom según tu preferencia
+    });
+
+    const marcador = new google.maps.Marker({
+        position: ubicacion,
+        map: mapa
+    });
+}
+
+function errorEnGeolocalizacion(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            alert('Se denegó la solicitud de geolocalización.');
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert('La información de ubicación no está disponible.');
+            break;
+        case error.TIMEOUT:
+            alert('Se agotó el tiempo para obtener la ubicación.');
+            break;
+        case error.UNKNOWN_ERROR:
+            alert('Ocurrió un error desconocido al obtener la ubicación.');
+            break;
+    }
+}
+
+obtenerUbicacion();
